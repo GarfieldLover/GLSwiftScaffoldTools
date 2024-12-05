7602788c7ab21ad1b9a891fa6d3ba8497adfcbd1
Ke Zhang <zhangke19880503@163.com>
2024-12-05 15:58:28 +0800

    init project

---
+++ SwiftMediator/SwiftMediator/SwiftMediator/TestVC.swift
@@ -0,0 +1,14 @@
+//
+//  TestVC.swift
+//  SwiftMediator
+//
+//
+
+import UIKit
+import SwiftBrick
+class TestVC: JHViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .random
+    }
+}
---
+++ SwiftMediator/SwiftMediator/SwiftMediator/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SwiftMediator
+//
+//
+
+import UIKit
+//import SwiftBrick
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let vc = SwiftMediator.shared.currentViewController()!
+        print(vc)
+        
+        let _ = UIButton.snpButton(supView: self.view, title: "push", snapKitMaker: { (make) in
+            make.top.equalToSuperview().offset(20)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(100)
+        }, touchUp: { (sender) in
+            let avc = SwiftMediator.shared.initVC(vcName: "SwiftBrick.JHViewController", dic: [:])!
+            SwiftMediator.shared.currentNavigationController()?.pushViewController(avc, animated: true)
+        }, backColor: .orange)
+        
+
+    }
+
+
+}
+
